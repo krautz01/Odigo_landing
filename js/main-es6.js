e6a75f0d15afe98cee02dab723e5d81d1f47eac6
krautz01 <85441603+krautz01@users.noreply.github.com>
2021-07-29 02:53:11 +0600

    Update main-es6.js

--- js/main-es6.js
+++ js/main-es6.js
@@ -1,11 +1,11 @@
 (function (){
     const header=document.querySelector('.header');
-    window.onscroll=() => {
+    window.onscroll= () => {
         if(window.pageYOffset > 50) {
-            header.classList.add ('header_active');
+            header.classList.add('header_active');
         }
         else{
-            header.classList.remove ('header_active');
+            header.classList.remove('header_active');
         }
     };
 }());
